6b56c517fe18e06cfa69b40cc69ec3c10ad1d863
Crew Spence <csspence123@gmail.com>
2020-10-17 07:12:24 -0500

    Created Binary Search Tree class.

---
+++ udemy_bst_exercise.js
@@ -0,0 +1,18 @@
+/*
+Udemy - JavaScript Algorithms and Data Structures Masterclass
+Binary Search Tree Class
+*/
+
+class Node {
+  constructor(value) {
+    this.value = value;
+    this.left = null;
+    this.right = null;
+  }
+}
+
+class BinarySearchTree {
+  constructor() {
+    this.root = null;
+  }
+}
