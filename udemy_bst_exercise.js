d28be2d85d4e4e5a3f44053062db085273c7827f
Crew Spence <csspence123@gmail.com>
2020-10-17 07:40:21 -0500

    Working on an insert method for BST.

--- udemy_bst_exercise.js
+++ udemy_bst_exercise.js
@@ -15,4 +15,34 @@ class BinarySearchTree {
   constructor() {
     this.root = null;
   }
+
+  insert(value) {
+    let node = new Node(value);
+    if(this.root = null) {
+      this.root = node;
+      return this;
+    }
+    let compareTo = this.root;
+    while(true) {
+      if(value === compareTo.value) {
+        return undefined;
+      }
+      if(value > compareTo.value) {
+        if(compareTo.right === null) {
+          compareTo.right = node;
+          return this;
+        } else {
+          compareTo = compareTo.right;
+        }
+      }
+      if(value < compareTo.value) {
+        if(compareTo.left === null) {
+         compareTo.left = node;
+          return this;
+        } else {
+          compareTo = compareTo.left;
+        }
+      }
+    }
+  }
 }
